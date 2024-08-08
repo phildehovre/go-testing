8598dc59546bef8a6040d2d855b250d3b2a77fbd
philippedehovre <ph.dehovre@gmail.com>
2024-08-08 15:04:43 +0100

    reflection - channels-maps-func

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -14,17 +14,72 @@ type Profile struct {
 	City string
 }
 
+// The idea is that the data keeps getting fed back into the Walk fn
+// It is stripped of a level of nesting every time, until it eventually
+// reaches to case reflect.String where it is passed to the function.
+
 func Walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			Walk(field.Interface(), fn)
+	walkValue := func(value reflect.Value) {
+		Walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
 		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for {
+			if v, ok := val.Recv(); ok {
+				walkValue(v)
+			} else {
+				break
+			}
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
+		}
+	}
+
+}
+
+func WalkAbstraction(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	var getField func(int) reflect.Value
+	numberOfValues := 0
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			WalkAbstraction(val.MapIndex(key).Interface(), fn)
+		}
+	}
+	for i := 0; i < numberOfValues; i++ {
+		WalkAbstraction(getField(i).Interface(), fn)
 	}
 }
 
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -39,6 +39,22 @@ func TestWalk(t *testing.T) {
 			"pointer to a struct",
 			&Person{"Chris", Profile{33, "London"}},
 			[]string{"Chris", "London"},
+		}, {
+			"slices",
+			[]Profile{
+				{22, "London"},
+				{33, "Paris"},
+				{44, "Bruxelles"},
+			},
+			[]string{"London", "Paris", "Bruxelles"},
+		},
+		{
+			"arrays",
+			[2]Person{
+				{"Chris", Profile{33, "London"}},
+				{"John", Profile{44, "Paris"}},
+			},
+			[]string{"Chris", "London", "John", "Paris"},
 		},
 	}
 
@@ -53,5 +69,74 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+
 	}
+	t.Run("with maps", func(t *testing.T) {
+
+		aMap := map[string]string{
+			"Cow":   "Moo",
+			"Sheep": "Baa",
+		}
+
+		var got []string
+
+		Walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Moo")
+		assertContains(t, got, "Baa")
+	})
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "London"}
+			aChannel <- Profile{44, "Bruxelles"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"London", "Bruxelles"}
+
+		Walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{44, "Brussels"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Brussels"}
+
+		Walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+
+	contains := false
+
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %v to contain %q but it did not", haystack, needle)
+	}
+
 }
