077f9256d2c2ea9ee5e11c58d8d638b1f9dd5207
philippedehovre <ph.dehovre@gmail.com>
2024-08-16 14:21:46 +0100

    maths

---
+++ maths/clockface.go
@@ -0,0 +1,34 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return float64(math.Pi / (30 / (float64(t.Second()))))
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+	return Point{x, y}
+}
